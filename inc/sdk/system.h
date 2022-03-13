68f893a85036fcd2d9b5e1d8ae34f3e976f098eb
Quinn Painter <quinn@geekymonkey.com>
2022-03-13 14:39:19 +0000

    Exclude unneccesary features
    is_system_gba is only needed if CGB seleted, and the SGB functions are only needed if building for SGB.

--- inc/sdk/system.h
+++ inc/sdk/system.h
@@ -4,11 +4,13 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define CPU_DMG 0x01 // Dot Matrix Game
-#define CPU_CGB 0x11 // Color GameBoy
-#define CPU_MGB 0xFF // Mini GameBoy (Pocket GameBoy)
+#define CPU_DMG 0x01 // Dot Matrix Game / Super GameBoy 1
+#define CPU_CGB 0x11 // Color GameBoy / Game Boy Advance
+#define CPU_MGB 0xFF // Mini GameBoy (Pocket GameBoy) / Super GameBoy 2
 
 extern uint8_t cpu_type; // DMG, CGB or MGB
+#if CGB
 extern bool system_is_gba; // Is the system a GBA or a CGB? (Only valid if cpu_type == CPU_CGB)
+#endif
 
 #endif//GBSDK_SYSTEM_H
