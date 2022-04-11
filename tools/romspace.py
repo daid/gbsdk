2ce53b388211e486f8581f51051ab0c5a8f8093b
Quinn Painter <quinn@geekymonkey.com>
2022-04-11 17:42:47 +0100

    Fix romspace when used on 32k roms linked with --tiny
    Also updated hardware.inc to fix version check macro

--- tools/romspace.py
+++ tools/romspace.py
@@ -5,6 +5,7 @@ SIZE = {"ROM0": 0x4000, "ROMX": 0x4000, "WRAM0": 0x2000, "HRAM": 0x007F, "VRAM":
 
 area_re = re.compile(r'([A-Z0-9]+) bank #([0-9a-f]+):\n')
 slack_re = re.compile(r'    SLACK: \$([0-9a-f]+) byte')
+areas = []
 for line in open(sys.argv[1], "rt"):
     area = area_re.match(line)
     if area:
@@ -12,5 +13,13 @@ for line in open(sys.argv[1], "rt"):
         bank_nr = int(area[2])
     slack = slack_re.match(line)
     if slack:
-        used = SIZE[area_name] - int(slack[1], 16)
-        print("%5s:%2d %04x/%04x (%.1f%%)" % (area_name, bank_nr, used, SIZE[area_name], used * 100 / SIZE[area_name]))
+        areas.append((area_name, bank_nr, int(slack[1], 16)))
+
+# Change size of ROM0 to 8000 if there are no ROMX sections.
+# Needed when using the '--tiny' option with RGBLINK.
+if not any([x for x in areas if x[0] == "ROMX"]):
+    SIZE["ROM0"] = 0x8000
+
+for area in areas:
+    used = SIZE[area[0]] - area[2]
+    print("%5s:%2d %04x/%04x (%.1f%%)" % (area[0], area[1], used, SIZE[area[0]], used * 100 / SIZE[area[0]]))
